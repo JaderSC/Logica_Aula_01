c4c86725811eebe192a81903884ce4439f3a5d8b
JaderSC <jader.sirqueira@hotmail.com>
2025-10-02 16:54:25 -0300

    Criação de IMC

---
+++ app.js
@@ -0,0 +1,8 @@
+
+
+
+let nomePaciente = "Marcos",
+    alturaPaciente = 1.95,
+    pesoPaciente = 90,
+    resultado = pesoPaciente / (alturaPaciente * alturaPaciente);
+    console.log("O Paciente " + nomePaciente + " Possui um IMC de: " + resultado.toFixed(1));
