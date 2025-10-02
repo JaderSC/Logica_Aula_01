8e5567b1e1bc9f5a8211603dfed571b85f161f19
JaderSC <jader.sirqueira@hotmail.com>
2025-10-02 17:21:23 -0300

    criado logica de avaliaçao do imc do paciente

--- app.js
+++ app.js
@@ -3,6 +3,16 @@
 
 let nomePaciente = "Marcos",
     alturaPaciente = 1.95,
-    pesoPaciente = 90,
+    pesoPaciente = 120,
     resultado = pesoPaciente / (alturaPaciente * alturaPaciente);
-    console.log("O Paciente " + nomePaciente + " Possui um IMC de: " + resultado.toFixed(1));
+//    console.log("O Paciente " + nomePaciente + " Possui um IMC de: " + resultado.toFixed(1));
+
+
+if (resultado < 18.5){
+
+    console.log("O Paciente " + nomePaciente + " Possui um IMC de: " + resultado.toFixed(1) + " Esta Abaixo do Peso");
+}else if (resultado >= 18.5 && resultado <= 24.9) {
+ console.log("O Paciente " + nomePaciente + " Possui um IMC de: " + resultado.toFixed(1) + " Esta com  Peso Normal");
+}else{
+    console.log("O Paciente " + nomePaciente + " Possui um IMC de: " + resultado.toFixed(1) + " Esta com  Peso Acima do Normal");
+}
